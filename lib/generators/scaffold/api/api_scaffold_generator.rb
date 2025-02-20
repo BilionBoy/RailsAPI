62d9ce9e627aa7ca59f161e232e99ef5f12f524a
Dhiogo <dhiogoandre26@gmail.com>
2025-02-20 11:05:52 -0400

    WIP: scope for api scaffold generator

--- lib/generators/scaffold/api/api_scaffold_generator.rb
+++ lib/generators/scaffold/api/api_scaffold_generator.rb
@@ -1,29 +1,23 @@
 # frozen_string_literal: true
 
 require "rails/generators"
+require_relative "scaffold_component_generator"
 
 module Scaffold
   module Api
     class ScaffoldGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path("templates", __dir__)       # Corrigindo o caminho do source_root
+      source_root File.expand_path("templates", __dir__)
 
+      # Argumento para atributos de campo
       argument :attributes, type: :array, default: [], banner: "field:type field:type"
-      
+
+      # Opção para escolher qual parte do scaffold gerar
       class_option :only, type: :string, default: nil, desc: "Escolha apenas uma parte: model, migration, controller"
 
+      # Método principal que decide o que gerar
       def generate_components
-        case options[:only]
-        when "model"
-          invoke "scaffold:api:model",      [name] + attributes
-        when "migration"
-          invoke "scaffold:api:migration",  [name] + attributes
-        when "controller"
-          invoke "scaffold:api:controller", [name] + attributes
-        else
-          invoke "scaffold:api:model",      [name] + attributes
-          invoke "scaffold:api:migration",  [name] + attributes
-          invoke "scaffold:api:controller", [name] + attributes
-        end
+        generator = Scaffold::Api::ScaffoldComponentGenerator.new(self, name, attributes, options)
+        generator.generate
       end
     end
   end
---
+++ lib/generators/scaffold/api/scaffold_component_generator.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Scaffold
+  module Api
+    class ScaffoldComponentGenerator
+      def initialize(generator, name, attributes, options)
+        @generator = generator
+        @name = name
+        @attributes = attributes
+        @options = options
+      end
+
+      def generate
+        case scaffold_component
+        when "model"
+          generate_model
+        when "migration"
+          generate_migration
+        when "controller"
+          generate_controller
+        else
+          generate_all_components
+        end
+      end
+
+      private
+
+      def scaffold_component
+        @options[:only].to_s.strip.downcase.presence || "all"
+      end
+
+      def generate_model
+        @generator.invoke "scaffold:api:model", [@name] + @attributes
+      end
+
+      def generate_migration
+        @generator.invoke "scaffold:api:migration", [@name] + @attributes
+      end
+
+      def generate_controller
+        @generator.invoke "scaffold:api:controller", [@name] + @attributes
+      end
+
+      def generate_all_components
+        generate_model
+        generate_migration
+        generate_controller
+      end
+    end
+  end
+end
